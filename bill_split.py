f761668037f91a459d6fdba2121929a97530b62e
waiwong-nc <WAIWONG.NC@gmail.com>
2022-12-21 10:19:14 +0000

    first commit

---
+++ bill_split.py
@@ -0,0 +1,9 @@
+print('---- Wilson \'s Bill Split Calculator  ----')
+people_number = int(input('How many people in the meal: '))
+price = float(input('How much is the meal: '))
+tip = float(input('How may tip you would like to give (%): '))
+total = price * (1+tip/100)
+share =  total / people_number
+print(f'Total of the bill is $ {total}')
+print(f'Each one should pay $ {share}')
+ 
